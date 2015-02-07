4a9c8e0b46022da9bf35993c3905134789fa6590
Matthew <matt.w.g.baldock@gmail.com>
2015-02-06 22:02:33 -0800

    The site IS the resume

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
 <link href="bootstrap-theme.min.css" rel="stylesheet">
 <link href="bootstrap.css" rel="stylesheet">
 <link href="bootstrap.min.css" rel="stylesheet">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-58312314-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+<style>
+#contentheader
+{
+font-size:20px;
+color:White;
+}
+#content
+{
+font-size:17px;
+}
+</style>
 <title>
 Matthew Baldock
 </title>
@@ -23,19 +24,95 @@ Matthew Baldock
 		<div class="container">
 		  <div class="jumbotron">
 		 
-            <h3>Welcome to MatthewBaldock.com, my own personal website. Follow the links below to see my projects or check out my LinkedIn page to see my credentials.</h3>
-			  <h3>
-			  Projects:<br>
-			<a href="http://www.mymediamemory.com" target="_blank"> My Media Memory </a><br>
-			<a href="http://www.yaketyup.com" target="_blank"> Yaketyup </a><br>
-			<a href="http://interactivephysicssimulator.matthewbaldock.com/" target="_blank"> Interactive Physics Simulator</a>
-			<br>
-			<br>
-			Profile:
-			<a href="https://www.linkedin.com/in/matthewbaldock">LinkedIn</a>
-			</h3>
-			<h4>Resume:<br></h4>
-		<embed height="1056" width="816"src="MatthewBaldockResume.pdf">
+            <h3>Welcome to MatthewBaldock.com, my own personal website. Follow the links below to see my projects or check out my <a href="https://www.linkedin.com/in/matthewbaldock" target="_blank">LinkedIn</a> page to see my credentials or my resume at the bottom.</h3>
+			<h2>Matthew Baldock</h2>
+			<h4>Pitt Meadows, BC<br>
+				(604) 446 – 4223(Mobile)<br>
+				Matt.w.g.baldock@gmail.com
+			</h4>
+			<h4>Career Objective</h4> 
+			<div id="content"> 
+			- Post-Secondary graduate looking for Part-Time, Full-Time, or Contract employment.
+			</div>
+			<h4>Education</h4> 
+			<div id="content"> 
+			<div id="contentheader">British Columbia Institute of Technology (Sept 2011 – Dec 2014)</div>
+			- Completed CST Program, Achieved Diploma in Computer Systems Technology with a focus in Technical Programming.
+			</div>
+			<h4>Work Experience</h4> 
+			<div id="content"> 
+			<div id="contentheader">COMP 4952/4956 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
+			Game Developer (BCIT Option Courses)<br>
+-	Developed a multiplayer online video game using XNA Framework. Personal contribution included game logic, game assets (graphics and sound) and coding UI logic.<br>
+-	Used Visual Studio, C# and GitHub as programming and versioning tools.<br>
+-	Participated in a team of 3 that was a subset of the large group of 12<br>
+-	Communicated with other teams and collaborated portions of the project to better merge the pieces.<br>
+-	Provided monthly reports of our progress to our instructor.<br>
+-	Gave a presentation about the whole project at the end of the term in front of other instructors and the President of BCIT
+</div><br>
+<div id="content"> 
+<div id="contentheader">COMP 4952 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
+Web Application (<a href="http://interactivephysicssimulator.matthewbaldock.com/" target="_blank"> Interactive Physics Simulator </a>)<br>
+-	Developed a ASP.NET Web application.<br>
+-	Used Visual Studio, C# and GitHub as programming and versioning tools.<br>
+-	Participated in a team of 2.<br>
+-	The Application is a graph written in JavaScript. There is a crosshair that can be moved with the mouse or inputs to produce a curved line that represents the flight of a projectile. - Different projectiles exist in the database.<br>
+-	Styled with Bootstrap.<br>
+-	Provided monthly reports of our progress to our instructor.<br>
+-	Gave a presentation about the whole project at the end of the term in front of our class.
+</div><br>
+<div id="content"> 
+<div id="contentheader">COMP 4711 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
+	Whistler Tourist Site (Intro to Web Development Course)<br>
+-	Created a web application using CodeIgniter as a tourist website for Whistler BC<br>
+-	Uses PHP, CodeIgniter, XML, Bootstrap.<br>
+-	Project lasted 4 months<br>
+-	Pair Programming project.<br>
+-	Created page templates that content loads into dynamically.<br>
+-	Admin page where tourist locations can be added.<br>
+-	Some content is loaded in from XML text in the database.
+</div><br>
+<div id="content"> 
+<div id="contentheader">Project Practicum 1 3900(Jan 2014 – April 2014) Burnaby, British Columbia</div>
+Project 1 (<a href="http://www.mymediamemory.com" target="_blank"> My Media Memory </a>)<br>
+-	Website to keep track of media consumption.<br>
+-	Uses PHP, Javascript, Ajax, API’s from third party Applications.<br>
+-	Personal contributions include, Friends System Logic, Friends Search, Friends Search Page Design, Profile Page Design, Notifications Logic and Notifications Box Design.<br>
+-	Had weekly client meetings, took notes, talked about client meetings with project manager.<br>
+-	Project lasted 4 months.<br>
+-	Provided weekly and monthly updates of progress.<br>
+-	Gave Presentation at the end of the course to client, project manager and instructors of the course.
+</div><br>
+<div id="content"> 
+<div id="contentheader">Project Practicum 2 4900(May 2014) Burnaby, British Columbia</div>
+			Project 2 (<a href="http://www.yaketyup.com" target="_blank"> Yaketyup </a>)<br>
+-	Website that surveys users on what kind of decisions they would make in certain situations<br>
+-	Uses PHP, Javascript, Ajax, Bootstrap.<br>
+-	Personal contributions include, Search Logic, Advanced Search Logic, Following Users Logic, Following Questions Logic.<br>
+-	Provided weekly and monthly updates of progress.<br>
+-	Had weekly client meetings, took notes, talked about client meetings with project manager.
+-	Project lasted 5 weeks.<br>
+-	Gave Presentation at the end of the course to client, project manager and instructors of the course.
+</div><br>
+<div id="content">
+<div id="contentheader">COMP 3952 Term Project (Jan 2014 – April 2014) Burnaby, British Columbia</div>
+		Visual Studio C# Software Application (Budget Scheduler)<br>
+-	Budget Scheduler is an application used for keeping track of expenses day to day and month to month.<br>
+-	Custom made calendar application to enter in expenses per day.<br>
+-	Monthly reports can be viewed to see how much has been spent.<br>
+-	Project lasted 4 months<br>
+-	Pair Programming project.<br>
+-	Uses XML to save and load expenses.<br>
+
+			</div>
+		<h4>Awards & Certificates  </h4>	
+			<div id="content">
+-	Diploma in Computer Systems Technology with a focus in Technical Programming.<br>
+-	BCIT Presidents Entry Scholarship (Worth 4,000$)<br>
+-	Double Dogwood, English and French (June 2011)
+
+
+			</div>
           </div>
 		  </div>
  <script src="jquery-1.11.1.min.js"></script>
--- index.php~
+++ index.php~
@@ -5,6 +5,16 @@
 <link href="bootstrap-theme.min.css" rel="stylesheet">
 <link href="bootstrap.css" rel="stylesheet">
 <link href="bootstrap.min.css" rel="stylesheet">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58312314-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <title>
 Matthew Baldock
 </title>
@@ -31,15 +41,5 @@ Matthew Baldock
  <script src="jquery-1.11.1.min.js"></script>
  <script src="bootstrap.js"></script>
  <script src="bootstrap.min.js"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-58312314-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
