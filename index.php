2d64689bf2798df1aba5f88aa7bf9d6b6b18bb7c
Matthew <matt.w.g.baldock@gmail.com>
2015-02-10 09:06:18 -0800

    dl types instead of br everywhere

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
 <link href="bootstrap.css" rel="stylesheet">
 <link href="bootstrap.min.css" rel="stylesheet">
 <style>
-#contentheader
+dt 
 {
-font-size:20px;
 color:White;
-}
-#content
-{
-font-size:17px;
 }
 </style>
 <title>
@@ -21,112 +16,114 @@ Matthew Baldock
 </title>
 </head>
 <body>
-		<div class="container">
-		  <div class="jumbotron">
+	<div class="container">
+		<div class="jumbotron">
 		 
-            <h3>Welcome to MatthewBaldock.com. Follow the links below to see my projects or check out my <a href="https://www.linkedin.com/in/matthewbaldock" target="_blank">LinkedIn</a> page to see my credentials or my resume at the bottom.</h3>
-			<h2>Matthew Baldock</h2>
-			<h4>Pitt Meadows, BC<br>
-				(604) 446 – 4223(Mobile)<br>
-				Matt.w.g.baldock@gmail.com
-			</h4>
-			<h4>Career Objective</h4> 
-			<div id="content"> 
-			- Post-Secondary graduate looking for Part-Time, Full-Time, or Contract employment.
-			</div>
-<h4>Skills & Abilities</h4> 
-			<div id="content"> 
-			-	Coding Languages: C, C++, C#, Java, CSS, HTML, JavaScript, PHP, XML, MySQL.<br>
--	IDEs: Visual Studio, Eclipse, NetBeans, Notepad++.<br>
--	Experience with: CodeIgniter, ASP.NET, Entity Framework, SOAP, REST XMLRPC<br>
--	Problem Solver, persists when faced with a difficult task.<br>
--	Ethical, would never do anything unfair or harmful to the company, customer or environment.<br>
--	Analytical.<br>
--	Team Player.<br>
--	Bilingual, speak and write English and French fluently.
+            <h3>Welcome to MatthewBaldock.com Follow the links below to see my projects or check out my <a href="https://www.linkedin.com/in/matthewbaldock" target="_blank">LinkedIn</a> page to see my credentials or my resume at the bottom.</h3>
+			<dl>
+				<dt><h5>Matthew Baldock</h5></dt>
+				
+					<dd>Pitt Meadows, BC</dd>
+					<dd>(604) 446 – 4223(Mobile)</dd>
+					<dd>Matt.w.g.baldock@gmail.com</dd>
+				
+				
+				<br>
+				<dt><h6>Career Objective</h6></dt> 
+				<dd>
+				- Post-Secondary graduate looking for Part-Time, Full-Time, or Contract employment.
+				</dd>
+			<br>
+			<dt><h6>Skills & Abilities</h6></dt>
+				<dd> - Coding Languages: C, C++, C#, Java, CSS, HTML, JavaScript, PHP, XML, MySQL.</dd>
+				<dd> - IDEs: Visual Studio, Eclipse, NetBeans, Notepad++, Android Studio, Titanium Studio.</dd>
+				<dd> - Experience with: CodeIgniter, ASP.NET, Entity Framework, SOAP, REST XMLRPC</dd>
+				<dd> - Problem Solver, persists when faced with a difficult task.</dd>
+				<dd> - Ethical, would never do anything unfair or harmful to the company, customer or environment.</dd>
+				<dd> - Analytical.</dd>
+				<dd> - Team Player.</dd>
+				<dd> -Bilingual, speak and write English and French fluently.</dd>
+			<br>
+			
+			<dt><h6>Education</h6></dt> 
+			<br>
+			 <dt>British Columbia Institute of Technology (Sept 2011 – Dec 2014)</dt>
+				<dd>- Completed CST Program, Achieved Diploma in Computer Systems Technology with a focus in Technical Programming.</dd>
+			<br>
+			<dt><h6>Work Experience</h6></dt> 
+			 <br>
+			<dt>COMP 4952/4956 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</dt>
+			<dt>Game Developer (BCIT Option Courses)</dt>
+				<dd> - Developed a multiplayer online video game using XNA Framework. Personal contribution included game logic, game assets (graphics and sound) and coding UI logic.</dd>
+				<dd> - Used Visual Studio, C# and GitHub as programming and versioning tools. </dd>
+				<dd> - Participated in a team of 3 that was a subset of the large group of 12 </dd>
+				<dd> - Communicated with other teams and collaborated portions of the project to better merge the pieces.</dd>
+				<dd> - Provided monthly reports of our progress to our instructor.</dd>
+				<dd> - Gave a presentation about the whole project at the end of the term in front of other instructors and the President of BCIT</dd>
+				<br>
+			<dt>COMP 4952 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</dt>
+			<dt> Web Application (<a href="http://interactivephysicssimulator.matthewbaldock.com/" target="_blank"> Interactive Physics Simulator </a>)</dt>
+				<dd> - Developed a ASP.NET Web application.</dd>
+				<dd> - Used Visual Studio, C# and GitHub as programming and versioning tools.</dd>
+				<dd> - Participated in a team of 2.</dd>
+				<dd> - The Application is a graph written in JavaScript. There is a crosshair that can be moved with the mouse or inputs to produce a curved line that represents the flight of a projectile.</dd>
+				<dd> - Different projectiles exist in the database.</dd>
+				<dd> - Styled with Bootstrap.</dd>
+				<dd> - Provided monthly reports of our progress to our instructor.</dd>
+				<dd> - Gave a presentation about the whole project at the end of the term in front of our class.</dd>
+				<br>
+			<dt>COMP 4711 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</dt>
+			<dt>Whistler Tourist Site (Intro to Web Development Course)</dt>
+				<dd> - Created a web application using CodeIgniter as a tourist website for Whistler BC</dd>
+				<dd> - Uses PHP, CodeIgniter, XML, Bootstrap.</dd>
+				<dd> - Project lasted 4 months</dd>
+				<dd> - Pair Programming project.</dd>
+				<dd> - Created page templates that content loads into dynamically.</dd>
+				<dd> - Admin page where tourist locations can be added.</dd>
+				<dd> - Some content is loaded in from XML text in the database.</dd>
+				<br>
+			<dt> Project Practicum 1 3900(Jan 2014 – April 2014) Burnaby, British Columbia</dt>
+			<dt>Project 1 (<a href="http://www.mymediamemory.com" target="_blank"> My Media Memory </a>)</dt>
+				<dd> - Website to keep track of media consumption.</dd>
+				<dd> - Uses PHP, Javascript, Ajax, API’s from third party Applications.</dd>
+				<dd> - Personal contributions include, Friends System Logic, Friends Search, Friends Search Page Design, Profile Page Design, Notifications Logic and Notifications Box Design.</dd>
+				<dd> - Had weekly client meetings, took notes, talked about client meetings with project manager.</dd>
+				<dd> - Project lasted 4 months.</dd>
+				<dd> - Provided weekly and monthly updates of progress.</dd>
+				<dd> - Gave Presentation at the end of the course to client, project manager and instructors of the course.</dd>
+				<br>
+				
+			<dt>Project Practicum 2 4900(May 2014) Burnaby, British Columbia</dt>
+			<dt>Project 2 (<a href="http://www.yaketyup.com" target="_blank"> Yaketyup </a>)</dt>
+				<dd> - Website that surveys users on what kind of decisions they would make in certain situations</dd>
+				<dd> - Uses PHP, Javascript, Ajax, Bootstrap.</dd>
+				<dd> - Personal contributions include, Search Logic, Advanced Search Logic, Following Users Logic, Following Questions Logic.</dd>
+				<dd> - Provided weekly and monthly updates of progress.</dd>
+				<dd> - Had weekly client meetings, took notes, talked about client meetings with project manager.</dd>
+				<dd> - Project lasted 5 weeks.</dd>
+				<dd> - Gave Presentation at the end of the course to client, project manager and instructors of the course.</dd>
+				<br>
 
-			</div><br>
-			<h4>Education</h4> 
-			<div id="content"> 
-			<div id="contentheader">British Columbia Institute of Technology (Sept 2011 – Dec 2014)</div>
-			- Completed CST Program, Achieved Diploma in Computer Systems Technology with a focus in Technical Programming.
-			</div>
-			<h4>Work Experience</h4> 
-			<div id="content"> 
-			<div id="contentheader">COMP 4952/4956 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
-			Game Developer (BCIT Option Courses)<br>
--	Developed a multiplayer online video game using XNA Framework. Personal contribution included game logic, game assets (graphics and sound) and coding UI logic.<br>
--	Used Visual Studio, C# and GitHub as programming and versioning tools.<br>
--	Participated in a team of 3 that was a subset of the large group of 12<br>
--	Communicated with other teams and collaborated portions of the project to better merge the pieces.<br>
--	Provided monthly reports of our progress to our instructor.<br>
--	Gave a presentation about the whole project at the end of the term in front of other instructors and the President of BCIT
-</div><br>
-<div id="content"> 
-<div id="contentheader">COMP 4952 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
-Web Application (<a href="http://interactivephysicssimulator.matthewbaldock.com/" target="_blank"> Interactive Physics Simulator </a>)<br>
--	Developed a ASP.NET Web application.<br>
--	Used Visual Studio, C# and GitHub as programming and versioning tools.<br>
--	Participated in a team of 2.<br>
--	The Application is a graph written in JavaScript. There is a crosshair that can be moved with the mouse or inputs to produce a curved line that represents the flight of a projectile. - Different projectiles exist in the database.<br>
--	Styled with Bootstrap.<br>
--	Provided monthly reports of our progress to our instructor.<br>
--	Gave a presentation about the whole project at the end of the term in front of our class.
-</div><br>
-<div id="content"> 
-<div id="contentheader">COMP 4711 Term Project (Sept 2014 – Dec 2014) Burnaby, British Columbia</div>
-	Whistler Tourist Site (Intro to Web Development Course)<br>
--	Created a web application using CodeIgniter as a tourist website for Whistler BC<br>
--	Uses PHP, CodeIgniter, XML, Bootstrap.<br>
--	Project lasted 4 months<br>
--	Pair Programming project.<br>
--	Created page templates that content loads into dynamically.<br>
--	Admin page where tourist locations can be added.<br>
--	Some content is loaded in from XML text in the database.
-</div><br>
-<div id="content"> 
-<div id="contentheader">Project Practicum 1 3900(Jan 2014 – April 2014) Burnaby, British Columbia</div>
-Project 1 (<a href="http://www.mymediamemory.com" target="_blank"> My Media Memory </a>)<br>
--	Website to keep track of media consumption.<br>
--	Uses PHP, Javascript, Ajax, API’s from third party Applications.<br>
--	Personal contributions include, Friends System Logic, Friends Search, Friends Search Page Design, Profile Page Design, Notifications Logic and Notifications Box Design.<br>
--	Had weekly client meetings, took notes, talked about client meetings with project manager.<br>
--	Project lasted 4 months.<br>
--	Provided weekly and monthly updates of progress.<br>
--	Gave Presentation at the end of the course to client, project manager and instructors of the course.
-</div><br>
-<div id="content"> 
-<div id="contentheader">Project Practicum 2 4900(May 2014) Burnaby, British Columbia</div>
-			Project 2 (<a href="http://www.yaketyup.com" target="_blank"> Yaketyup </a>)<br>
--	Website that surveys users on what kind of decisions they would make in certain situations<br>
--	Uses PHP, Javascript, Ajax, Bootstrap.<br>
--	Personal contributions include, Search Logic, Advanced Search Logic, Following Users Logic, Following Questions Logic.<br>
--	Provided weekly and monthly updates of progress.<br>
--	Had weekly client meetings, took notes, talked about client meetings with project manager.
--	Project lasted 5 weeks.<br>
--	Gave Presentation at the end of the course to client, project manager and instructors of the course.
-</div><br>
-<div id="content">
-<div id="contentheader">COMP 3952 Term Project (Jan 2014 – April 2014) Burnaby, British Columbia</div>
-		Visual Studio C# Software Application (Budget Scheduler)<br>
--	Budget Scheduler is an application used for keeping track of expenses day to day and month to month.<br>
--	Custom made calendar application to enter in expenses per day.<br>
--	Monthly reports can be viewed to see how much has been spent.<br>
--	Project lasted 4 months<br>
--	Pair Programming project.<br>
--	Uses XML to save and load expenses.<br>
+			<dt>COMP 3952 Term Project (Jan 2014 – April 2014) Burnaby, British Columbia</dt>
+			<dt>Visual Studio C# Software Application (Budget Scheduler)</dt>
+				<dd> - Budget Scheduler is an application used for keeping track of expenses day to day and month to month.</dd>
+				<dd> - Custom made calendar application to enter in expenses per day.</dd>
+				<dd> - Monthly reports can be viewed to see how much has been spent.</dd>
+				<dd> - Project lasted 4 months</dd>
+				<dd> - Pair Programming project.</dd>
+				<dd> - Uses XML to save and load expenses.</dd>
+				<br>
+				
+			<dt><h6>Awards & Certificates</h6></dt>	
+			<br>
+				<dd> - Diploma in Computer Systems Technology with a focus in Technical Programming.</dd>
+				<dd> - BCIT Presidents Entry Scholarship (Worth 4,000$)</dd>
+				<dd> - Double Dogwood, English and French (June 2011)</dd>
+		</dl>
 
-			</div>
-		<h4>Awards & Certificates  </h4>	
-			<div id="content">
--	Diploma in Computer Systems Technology with a focus in Technical Programming.<br>
--	BCIT Presidents Entry Scholarship (Worth 4,000$)<br>
--	Double Dogwood, English and French (June 2011)
-
-
-			</div>
-          </div>
-		  </div>
+		</div>
+    </div>
+		  
  <script src="jquery-1.11.1.min.js"></script>
  <script src="bootstrap.js"></script>
  <script src="bootstrap.min.js"></script>
