96008b368c660d5c42472b3f290da74fc6bd7d37
Matthew <matt.w.g.baldock@gmail.com>
2015-02-13 18:32:58 -0800

    Added super secret link

---
+++ supersecretlinkindexofsecrecy.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>
+ Super Secret
+</title>
+<style>
+
+</style>
+</head>
+<body style="background-color:pink; background-image:url('giphy.gif');">
+
+<div style="text-align: center;">
+<img src="gushy_text.png"/>
+<img src="gushy.png"/>
+</div>
+</body>
+
+
+</html>
