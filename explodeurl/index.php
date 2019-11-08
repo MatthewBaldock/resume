9cc9e3c46b3b233f600e16a855ab58aea31df7c0
Matthew <matt.w.g.baldock@gmail.com>
2019-11-08 11:11:42 -0800

    ads

---
+++ explodeurl/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Explore URL</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+                <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58312314-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-58312314-3');
+</script>
+    </head>
+    <body>
+        <div class="container">
+        <h2>ExplodeURL.xyz</h2>
+        <div>Don't have trustworthy friends? Don't trust your friends? Have a URL from a sketchy website?</div>
+        <div>Test your shortened URL to see where it goes!</div>
+        <div>
+            <label for="shortURL">
+                Enter your shortened URL
+            </label>
+            <input id="shortURL" type="text" title="URL"/>
+            <input id="subURL" type="submit" value="Submit"/>
+            <span id="urlERR" class="label label-danger"></span>
+        </div>
+        <div id="responseText">
+            
+        </div>
+        </div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ExplodeAd -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1075230236221127"
+     data-ad-slot="3157226862"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </body>
+    <footer>
+        <script src ="https://matthewbaldock.000webhostapp.com/explodeurl/js/myScript.js" type="text/javascript"></script>
+
+    </footer>
+</html>
